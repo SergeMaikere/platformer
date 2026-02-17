b79454f1cacde3f9ba038c7ce7ab5e5749200c9a
SergeMaikere <serge.maikere@gmail.com>
2026-02-17 17:27:41 +0100

    + Worm movements

--- Entities/Worm.py
+++ Entities/Worm.py
@@ -1,12 +1,23 @@
 from pygame import Surface
+from pygame.rect import FRect
 from pygame.sprite import Group
 from settings import *
 from Utils.Entity import Entity
 
 class Worm ( Entity ):
-	def __init__(self, frames: list[Surface], *groups: Group, **anchor: tuple[float, float]) -> None:
-		super().__init__(frames, *groups, **anchor)
+	def __init__(self, frames: list[Surface], patrol_area: FRect, *groups: Group) -> None:
+		super().__init__(frames, *groups, bottomleft=patrol_area.bottomleft)
 
-	def update ( self, dt: float ):
-		self._animate(dt)
-		# self._move(dt)
+		self.patrol_area = patrol_area
+
+		self.speed = 150
+		self.direction.x = 1
+
+	def _set_direction(self):
+		if not self.patrol_area.contains(self.rect): self.direction.x *= -1
+
+	def _constraint(self):
+		if not self.patrol_area.contains(self.rect): 
+			self.frames = [ pygame.transform.flip(frame, True, False) for frame in self.frames ]
+		
+	
